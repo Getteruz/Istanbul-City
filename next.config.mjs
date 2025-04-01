6ceffc4c65dadae933c828ddd216046a5882a6e9
Getter IT Company <130207894+Getteruz@users.noreply.github.com>
2025-04-01 19:56:23 +0000

    Update Next.js config for static export

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    "domains": ['grm-upload.getter.uz']
-  }
+    domains: ['grm-upload.getter.uz'],
+  },
+  trailingSlash: true,  // Для добавления слеша в конце URL (полезно при экспорте статических файлов)
+  
+  // Настройки для статического экспорта
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }, // Главная страница
+      // Здесь ты можешь добавить другие страницы, если нужно
+    };
+  },
 };
 
 export default nextConfig;
